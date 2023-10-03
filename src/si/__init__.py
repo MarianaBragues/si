05569355fb815897de25fb3f6dc8cc455467c586
MarianaBragues <120012178+MarianaBragues@users.noreply.github.com>
2023-10-03 16:00:31 +0100

    changes_first_class

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "João Correia"
-__credits__ = ["João Correia, Fernando Cruz", "Vítor Pereira"]
+__author__ = "Mariana Braguês"
+__credits__ = ["Mariana Braguês"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "João Correia"
-__email__ = "jfscorreia95@gmail.com"
+__maintainer__ = "Mariana Braguês"
+__email__ = "marianabragues@hotmail.com"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
