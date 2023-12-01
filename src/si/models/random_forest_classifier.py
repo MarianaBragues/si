123f1ca7cd96e8631ba9a8dc406a13eff42823e4
MarianaBragues <120012178+MarianaBragues@users.noreply.github.com>
2023-12-01 18:16:01 +0000

    random_forest_classifier finished

--- src/si/models/random_forest_classifier.py
+++ src/si/models/random_forest_classifier.py
@@ -1,50 +1,62 @@
+from typing import Literal
 import numpy as np
 from si.data.dataset import Dataset
-from si.models.decision_tree_classifier import Node, DecisionTreeClassifier
 from si.metrics.accuracy import accuracy
+from si.models.decision_tree_classifier import DecisionTreeClassifier
 
 class RandomForestClassifier:
     """
-    Random Forest is an ensemble machine learning technique that combines multiple decision trees to improve prediction 
-    accuracy and reduce overfitting.
-
-    
-    parameters:
-    -----------
-    n_estimators: 
-        number of decision trees to use
-    
-    max_features:
-        maximum number of features to use per tree
-    
-    min_sample_split:
-        minimum samples allowed in a split
-
-    max_depth:
-        maximum depth of the trees
-
-    mode:
-        impurity calculation mode (gini or entropy)
-
-    seed:
-        random seed to use to assure reproducibility
-
-        
-    estimated parameters:
-    ---------------------
-    trees: list
-        the trees of the random forest and respective features used for training (initialized as an empty list)
+    Ensemble machine learning technique that combines multiple decision trees to improve prediction accuracy 
+    and reduce overfitting
     """
-    def __init__(self, n_estimators=100, max_features=None, min_sample_split=2, max_depth=None, mode = 'gini', seed=None):
+
+    def __init__(self, n_estimators:int = 100, 
+                 max_features:int = None,
+                 min_sample_split:int = 2,
+                 max_depth:int = 15,
+                 mode: Literal['gini','entropy'] = 'gini',
+                 seed:int = None):
+        """
+        Random Forest is an ensemble machine learning technique that combines multiple decision trees to improve prediction 
+        accuracy and reduce overfitting.
+    
+        parameters:
+        -----------
+        n_estimators: 
+            number of decision trees to use
+        
+        max_features:
+            maximum number of features to use per tree
+        
+        min_sample_split:
+            minimum samples allowed in a split
+
+        max_depth:
+            maximum depth of the trees
+
+        mode:
+            impurity calculation mode (gini or entropy)
+
+        seed:
+            random seed to use to assure reproducibility
+
+        estimated parameters:
+        ---------------------
+        trees: list
+            the trees of the random forest and respective features used for training (initialized as an empty list)
+        """
+        #attributes
         self.n_estimators = n_estimators
         self.max_features = max_features
         self.min_sample_split = min_sample_split
         self.max_depth = max_depth
         self.mode = mode
         self.seed = seed
-        self.trees = []
 
-    
+        #parameters
+        self.trees = []
+        self.training = {}
+
     def set_random_seed(self):
         """
         Set a random seed for NumPy
@@ -56,29 +68,8 @@ class RandomForestClassifier:
         if self.seed is not None:
             np.random.seed(self.seed)
 
-    def get_bootstrap_dataset(self, dataset: Dataset):
-        """
-        Generate a bootstrap dataset with randomly sampled instances and features from the original dataset. 
-        
-        Parameters
-        ----------
-        dataset: Dataset
-            The dataset to fit the model to
 
-        Returns
-        -------
-        self: 
-            The fitted model
-        """
-        n_samples, n_features = dataset.shape()
-
-        indices = np.random.choice(n_samples, n_samples, replace=True)
-        features = np.random.choice(n_features, self.max_features, replace=False)
-        
-        return Dataset(X=dataset.X[indices][:, features], y=dataset.y[indices])
-
-
-    def fit(self, dataset: Dataset) -> 'RandomForestClassifier':
+    def fit(self, dataset:Dataset)->'RandomForestClassifier':
         """
         Train the decision trees of the random forest
 
@@ -93,48 +84,50 @@ class RandomForestClassifier:
             The fitted model
         """
         self.set_random_seed()
-        n_features = dataset.shape()[1]
-        self.max_features = int(np.sqrt(n_features)) if self.max_features is None else self.max_features
+            
+        n_samples, n_features = dataset.shape()
+        if self.max_features is None:
+            self.max_features = int(np.sqrt(n_features)) 
 
-        if self.max_depth is None:
-            self.max_depth = 10
+        #creating a bootstrap 
+        for x in range(self.n_estimators):  
+            bootstrap_samples = np.random.choice(n_samples, n_samples, replace = True) 
+            bootstrap_features = np.random.choice(n_features, self.max_features, replace=False) 
+            
+            random_dataset = Dataset(dataset.X[bootstrap_samples][:,bootstrap_features], dataset.y[bootstrap_samples])
+        
+            tree = DecisionTreeClassifier(min_sample_split=self.min_sample_split, max_depth=self.max_depth, mode = self.mode)
 
-        for _ in range(self.n_estimators):
-            bootstrap_dataset = self.get_bootstrap_dataset(dataset)
-            tree = DecisionTreeClassifier(min_sample_split=self.min_sample_split,
-                                          max_depth=self.max_depth,
-                                          mode=self.mode)
-            tree.fit(bootstrap_dataset)
-            self.trees.append((bootstrap_dataset.features, tree))
+            tree.fit(random_dataset)
+
+            self.trees.append((bootstrap_features, tree))  
 
         return self
     
-
-    def predict(self, dataset: Dataset) -> np.array:
+    def predict(self, dataset:Dataset)-> np.ndarray:
         """
-        Predicts the labels using the ensemble models
+        Predicts the class labels for a dataset.
 
         Parameters
         ----------
         dataset: Dataset
-            The dataset to predict the output of
+            The dataset for which to make predictions.
 
         Returns
         -------
-        predictions: np.array
-            The predictions of the dataset
+        np.ndarray
+            An array of predicted class labels.
         """
-        #self.dataset = dataset  
-        all_predictions = []
+        n_samples = dataset.shape()[0]
+        predictions = np.zeros((self.n_estimators, n_samples), dtype=object) 
+        
+        for tree_idx, (feature_idx, tree) in enumerate(self.trees):
+            data_samples = Dataset(dataset.X[:, feature_idx], dataset.y)  # Subset the dataset based on tree's features
+            tree_preds = tree.predict(data_samples)  # Get predictions from the tree
+            predictions[tree_idx, :] = tree_preds  # Store predictions for the current tree
 
-        for _, tree in self.trees:
-            X_subset = dataset.X[:, tree.feature_idx]
-            predictions = tree.predict(Dataset(X=X_subset))
-            all_predictions.append(predictions)
+        return predictions
 
-        all_predictions = np.array(all_predictions).T
-        return np.array([np.argmax(np.bincount(sample)) for sample in all_predictions])
-    
 
     def score(self, dataset: Dataset) -> float:
         """
@@ -150,5 +143,6 @@ class RandomForestClassifier:
         random_forest: float
             The Mean Square Error of the model
         """
-        return accuracy(dataset.y, self.predict(dataset))
+        predictions = self.predict(dataset)
+        return accuracy(dataset.y, predictions)
     
