9235538b65aa99abd02b91db8bfb7db2bac5385d
MarianaBragues <120012178+MarianaBragues@users.noreply.github.com>
2023-12-05 17:15:14 +0000

    select_k_best descrito

--- src/si/feature_selection/select_k_best.py
+++ src/si/feature_selection/select_k_best.py
@@ -28,6 +28,7 @@ class SelectKBest:
         p-values of F-scores.
     """
     def __init__(self, score_func: Callable = f_classification, k: int = 10):
+        #define o construtor com os parâmetros iniciais
         """
         Select features according to the k highest scores.
 
@@ -38,10 +39,11 @@ class SelectKBest:
         k: int, default=10
             Number of top features to select.
         """
-        self.k = k
-        self.score_func = score_func
+        self.k = k #número de features a serem selecionadas, by default é 10
+        self.score_func = score_func #calcula algum tipo de pontuação ou valor para as features, by default é f_classification
         self.F = None
         self.p = None
+        #as variáveis F e p são inicializadas como None
 
     def fit(self, dataset: Dataset) -> 'SelectKBest':
         """
@@ -57,8 +59,9 @@ class SelectKBest:
         self: object
             Returns self.
         """
-        self.F, self.p = self.score_func(dataset)
-        return self
+        self.F, self.p = self.score_func(dataset) #chama a função de pontuação (score_func), passando o dataset fornecido 
+        #como argumento. Esta função devolve os valores de pontuação de F e p para as features no dataset. 
+        return self #devolve o dataset com as novas alterações
 
     def transform(self, dataset: Dataset) -> Dataset:
         """
@@ -74,9 +77,14 @@ class SelectKBest:
         dataset: Dataset
             A labeled dataset with the k highest scoring features.
         """
-        idxs = np.argsort(self.F)[-self.k:]
-        features = np.array(dataset.features)[idxs]
-        return Dataset(X=dataset.X[:, idxs], y=dataset.y, features=list(features), label=dataset.label)
+        idxs = np.argsort(self.F)[-self.k:] #usa np.argsort para obter os índices das K features de maior pontuação nos 
+        #valores de F armazenados em F
+        features = np.array(dataset.features)[idxs] #usa os índices selecionados para obter os nomes das features 
+        #correspondentes no dataset de entrada
+        return Dataset(X=dataset.X[:, idxs], y=dataset.y, features=list(features), label=dataset.label) #devolve um novo 
+        #dataset contendo apenas as features selecionadas com base nos índices obtidos. Seleciona as colunas correspondentes
+        #nos dados de entrada X, atualiza as labels das features para as selecionadas, mantém as labels y e label no novo 
+        #conjunto de dados.
 
     def fit_transform(self, dataset: Dataset) -> Dataset:
         """
@@ -92,10 +100,12 @@ class SelectKBest:
         dataset: Dataset
             A labeled dataset with the k highest scoring features.
         """
-        self.fit(dataset)
-        return self.transform(dataset)
+        self.fit(dataset) #chama o método fit desta classe que calcula os valores de pontuação de F para as features no 
+        #dataset fornecido
+        return self.transform(dataset) #chama o método transform desta classe e seleciona as features com base nos valores
+        #de F calculados anteriormente. Devolve um novo dataset contendo apenas as K features de maior pontuação
 
-
+#Testes:
 if __name__ == '__main__':
     from si.data.dataset import Dataset
 
