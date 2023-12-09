be6fbe223c4b56f988e9e4b2182e60e22371dadc
MarianaBragues <120012178+MarianaBragues@users.noreply.github.com>
2023-12-09 17:56:30 +0000

    variance_threshold descrito

--- src/si/feature_selection/variance_threshold.py
+++ src/si/feature_selection/variance_threshold.py
@@ -1,5 +1,5 @@
+#importação dos packages necessários:
 import numpy as np
-
 from si.data.dataset import Dataset
 
 
@@ -21,6 +21,7 @@ class VarianceThreshold:
     """
 
     def __init__(self, threshold: float = 0.0):
+        #definição do construtor:
         """
         Variance Threshold feature selection.
         Features with a training-set variance lower than this threshold will be removed from the dataset.
@@ -38,7 +39,7 @@ class VarianceThreshold:
         self.threshold = threshold
 
         # attributes
-        self.variance = None
+        self.variance = None #inicia a variância como None
 
     def fit(self, dataset: Dataset) -> 'VarianceThreshold':
         """
@@ -52,6 +53,8 @@ class VarianceThreshold:
         -------
         self : object
         """
+        #calculo da variância das features do dataset de treino, através da função var do NumPy, onde axis=0 indica que a 
+        #variância deve ser calculada ao longo do eixo das colunas. O resultado é armazenado no atributo variance:
         self.variance = np.var(dataset.X, axis=0)
         return self
 
@@ -66,12 +69,18 @@ class VarianceThreshold:
         -------
         dataset: Dataset
         """
-        X = dataset.X
+        X = dataset.X #atribui o conjunto de dados de features (X) do dataset à variável X
 
-        features_mask = self.variance > self.threshold
-        X = X[:, features_mask]
-        features = np.array(dataset.features)[features_mask]
+        features_mask = self.variance > self.threshold #cria uma mask onde os valores correspondem a True se a variância 
+        #da feature for maior que o threshold
+        X = X[:, features_mask] #usa a mask para selecionar apenas as features cuja variância atende ao threshold, 
+        #atualizando o dataset X apenas com essas features
+        features = np.array(dataset.features)[features_mask] #seleciona as features correspondentes à mask, criando um 
+        #novo conjunto de features
+        #devolve um novo Dataset com as features selecionadas, mantendo as labels (y), a lista de features e a label do 
+        #dataset original, devolve um dataset transformado:
         return Dataset(X=X, y=dataset.y, features=list(features), label=dataset.label)
+
 
     def fit_transform(self, dataset: Dataset) -> Dataset:
         """
@@ -84,8 +93,9 @@ class VarianceThreshold:
         -------
         dataset: Dataset
         """
-        self.fit(dataset)
-        return self.transform(dataset)
+        self.fit(dataset) #chama a função fit, ajustando o modelo aos dados de entrada dataset
+        return self.transform(dataset) #devolve o resultado da transformação aplicada ao dataset fornecido. 
+        #Chama a função transform para aplicar a transformação ao dataset após ter sido ajustado pelo método fit
 
 
 if __name__ == '__main__':
