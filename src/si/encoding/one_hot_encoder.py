e0524cd82580925cb3f5b4ad24f2098b31c6adc8
MarianaBragues <120012178+MarianaBragues@users.noreply.github.com>
2023-12-09 17:23:57 +0000

    one_hot_encoder descrito

--- src/si/encoding/one_hot_encoder.py
+++ src/si/encoding/one_hot_encoder.py
@@ -1,3 +1,4 @@
+#importação dos packages necessários:
 from typing import List, Optional, Dict
 import numpy as np
 
@@ -9,6 +10,7 @@ class OneHotEncoder:
     specific category) and the rest set to 0.
     """
     def __init__(self, padder: Optional[str] =None, max_length: Optional[int] =None):
+        #definir o construtor
         """
         Parameters
         ----------
@@ -35,6 +37,7 @@ class OneHotEncoder:
         self.char_to_index: Optional[Dict[str, int]] = None
         self.index_to_char: Optional[Dict[int, str]] = None
 
+
     def fit(self, data: list) -> None:
         """
         Fits the encoder to the data (learns the alphabet , char_to_index and index_to_char) + sets max_length if not 
@@ -45,13 +48,21 @@ class OneHotEncoder:
         data: list
             list of sequences (strings) to learn from
         """
+        #cria o atributo alphabet, que contém os caracteres únicos presentes em todas as sequências fornecidas em data. 
+        #set(''.join(data)) cria uma string única combinando todas as sequências em data, e sorted ordena os caracteres únicos:
         self.alphabet = sorted(set(''.join(data)))
+
+        #cria um dicionário char_to_index que mapeia cada caracter do alphabet para um índice único. 
+        #enumerate gera pares índice-caracteres para cada caracter no alphabet:
         self.char_to_index = {char: index for index, char in enumerate(self.alphabet)}
+
+        #cria um dicionário index_to_char que mapeia cada índice único de volta para o seu caracter correspondente no 
+        #alphabet:
         self.index_to_char = {index: char for index, char in enumerate(self.alphabet)}
 
-        # Set max_length if not defined
-        if self.max_length is None:
-            self.max_length = max(len(seq) for seq in data)
+        if self.max_length is None: #verifica se max_length não está definido
+            self.max_length = max(len(seq) for seq in data) #se max_length não estiver definido, determina o valor máximo
+            #de comprimento das sequências em data e atribui esse valor a max_length
 
 
     def transform(self, data: List[str]) -> List[np.ndarray]:
@@ -63,22 +74,23 @@ class OneHotEncoder:
         data: list 
             data to encode
         """
-        encoded_sequences = []
-        for sequence in data:
-            # Trim or pad sequences to max_length
-            if len(sequence) > self.max_length:
-                sequence = sequence[:self.max_length]
-            elif len(sequence) < self.max_length:
-                sequence = sequence + (self.padder * (self.max_length - len(sequence)))
+        encoded_sequences = [] #inici uma lista vazia para armazenar as sequências codificadas - encoded_sequences
+        for sequence in data: #itera sobre cada sequência em data:
+            if len(sequence) > self.max_length: #verifica se o comprimento da sequência atual é maior que max_length
+                sequence = sequence[:self.max_length] #se a sequência for maior que max_length, é cortada para o comprimento máximo
+            elif len(sequence) < self.max_length: #verifica se o comprimento da sequência é menor que max_length
+                sequence = sequence + (self.padder * (self.max_length - len(sequence))) #se a sequência for menor que 
+                #max_length, é preenchida com padder até atingir o comprimento máximo
 
-            # Encode the sequence to one-hot encoding
-            encoded_sequence = np.zeros((self.max_length, len(self.alphabet)))
-            for i, char in enumerate(sequence):
-                if char in self.char_to_index:
-                    encoded_sequence[i, self.char_to_index[char]] = 1
-            encoded_sequences.append(encoded_sequence)
+            encoded_sequence = np.zeros((self.max_length, len(self.alphabet))) #inicializa uma matriz de zeros para 
+            #armazenar a representação one-hot da sequência. A matriz tem o tamanho (max_length, len(alphabet))
+            for i, char in enumerate(sequence): #itera sobre cada caracter na sequência:
+                if char in self.char_to_index: #verifica se o caracter está presente no dicionário char_to_index
+                    encoded_sequence[i, self.char_to_index[char]] = 1 #define o valor correspondente ao índice do 
+                    #caractere como 1
+            encoded_sequences.append(encoded_sequence) #adiciona a representação one-hot da sequência à lista de sequências codificadas
 
-        return encoded_sequences
+        return encoded_sequences #devolve a lista de sequências codificadas em representações one-hot
     
 
     def fit_transform(self, data: List[str]) -> List[np.ndarray]:
@@ -90,8 +102,8 @@ class OneHotEncoder:
         data: List[str]
             list of sequences (strings) to learn from
         """
-        self.fit(data)
-        return self.transform(data)
+        self.fit(data) #executa a etapa de ajuste após chamar a função fit
+        return self.transform(data) #devolve os dados transformados após chamar a função transform
 
 
     def inverse_transform(self, encoded_sequences: List[np.ndarray]) -> List[str]:
@@ -103,8 +115,42 @@ class OneHotEncoder:
         encoded_sequences: List[np.ndarray]
             data to decode one hot encoded matrices
         """
-        decoded_sequences = []
-        for encoded_sequence in encoded_sequences:
+        decoded_sequences = [] #inicia uma lista vazia para armazenar as sequências descodificadas
+        for encoded_sequence in encoded_sequences: #itera sobre cada matriz codificada one-hot:
+            #para cada matriz, encontra o índice do valor máximo em cada linha (axis=1), obtendo assim o índice de cada 
+            #caracter e usa esses índices para obter o caracter correspondente do dicionário index_to_char
+            #junta os caracteres para formar a sequência descodificada:
             decoded_sequence = ''.join([self.index_to_char[index] for index in encoded_sequence.argmax(axis=1)])
-            decoded_sequences.append(decoded_sequence)
-        return decoded_sequences
+            decoded_sequences.append(decoded_sequence) #adiciona a sequência descodificada à lista
+
+        return decoded_sequences #devolve a lista de sequências descodificadas
+    
+#Testes:
+if __name__ == "__main__":
+    #criar uma instância da classe OneHotEncoder
+    encoder = OneHotEncoder()
+
+    #dados de exemplo
+    data = ["abc", "def", "ghi"]
+
+    #Testa o método fit
+    print("Fit data:")
+    encoder.fit(data)
+    print("Alphabet:", encoder.alphabet)
+    print("Char to Index:", encoder.char_to_index)
+    print("Index to Char:", encoder.index_to_char)
+
+    #Testa o método transform
+    print("\nTransformed data:")
+    encoded_data = encoder.transform(data)
+    for idx, seq in enumerate(encoded_data):
+        print(f"Sequence {idx + 1}:", seq)
+
+    #Testa o método inverse_transform
+    print("\nInverse Transformed data:")
+    decoded_data = encoder.inverse_transform(encoded_data)
+    for idx, seq in enumerate(decoded_data):
+        print(f"Decoded Sequence {idx + 1}:", seq)
+
+    #Verifica se os dados originais são iguais aos dados decodificados
+    assert data == decoded_data, "Erro: Os dados decodificados não correspondem aos dados originais."
