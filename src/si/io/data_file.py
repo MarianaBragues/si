184fa2672c262d7a2b5b48978ed688e2c3669f6d
MarianaBragues <120012178+MarianaBragues@users.noreply.github.com>
2023-12-09 18:04:16 +0000

    data_file descrito

--- src/si/io/data_file.py
+++ src/si/io/data_file.py
@@ -1,5 +1,5 @@
+#importação dos packages necessários:
 import numpy as np
-
 from si.data.dataset import Dataset
 
 
@@ -23,17 +23,18 @@ def read_data_file(filename: str,
     Dataset
         The dataset object
     """
-    raw_data = np.genfromtxt(filename, delimiter=sep)
+    raw_data = np.genfromtxt(filename, delimiter=sep) #lê o arquivo especificado por filename usando a função genfromtxt 
+    #do NumPy para carregar os dados. Usa delimiter=sep para definir o separador dos dados no arquivo
 
-    if label:
-        X = raw_data[:, :-1]
-        y = raw_data[:, -1]
+    if label: #verifica se a label é verdadeira:
+        X = raw_data[:, :-1] #se a label for verdadeira, atribui a X todas as colunas do raw_data, exceto a última
+        y = raw_data[:, -1] #se a label for verdadeira, atribui a y a última coluna do raw_data
 
-    else:
-        X = raw_data
-        y = None
+    else: #se a label for falsa:
+        X = raw_data #atribui a X todos os dados 
+        y = None #y é None
 
-    return Dataset(X, y)
+    return Dataset(X, y) #devolve o dataset com os dados X e as labels y
 
 
 def write_data_file(filename: str,
@@ -54,12 +55,15 @@ def write_data_file(filename: str,
     label : bool, optional
         Whether to write the file with label, by default False
     """
-    if not sep:
-        sep = " "
+    if not sep: #verifica se sep é falso
+        sep = " " #define sep como um espaço em branco
 
-    if label:
-        data = np.hstack((dataset.X, dataset.y.reshape(-1, 1)))
-    else:
-        data = dataset.X
+    if label: #se a label é verdadeira
+        data = np.hstack((dataset.X, dataset.y.reshape(-1, 1))) #se label for verdadeira, concatena as colunas do dataset.X
+        #com dataset.y, que é redimensionado para se tornar numa matriz de uma coluna. Esta concatenação ocorre 
+        #horizontalmente usando a função hstack do NumPy
+    else: #se a label é falsa
+        data = dataset.X #define data como os dados do dataset.X
 
-    return np.savetxt(filename, data, delimiter=sep)
+    return np.savetxt(filename, data, delimiter=sep) #usa a função savetxt do NumPy para guardar os dados data no arquivo 
+    #especificado por filename, usando o separador sep
