db8746689845aac874d165564dfcc6b4ca807050
MarianaBragues <120012178+MarianaBragues@users.noreply.github.com>
2023-12-09 18:15:05 +0000

    csv descrito

--- src/si/io/csv_file.py
+++ src/si/io/csv_file.py
@@ -1,5 +1,5 @@
+#importação dos packages necessários:
 import pandas as pd
-
 from si.data.dataset import Dataset
 
 
@@ -26,32 +26,33 @@ def read_csv(filename: str,
     Dataset
         The dataset object
     """
-    data = pd.read_csv(filename, sep=sep)
+    data = pd.read_csv(filename, sep=sep) #lê o arquivo CSV usando a biblioteca Pandas e armazena o conteúdo na variável 
+    #data. O separador utilizado é o especificado por sep
 
-    if features and label:
-        features = data.columns[:-1]
-        label = data.columns[-1]
-        X = data.iloc[:, :-1].to_numpy()
-        y = data.iloc[:, -1].to_numpy()
+    if features and label: #se features e label forem verdadeiras:
+        features = data.columns[:-1] #define features como as colunas do DataFrame, excluindo a última coluna
+        label = data.columns[-1] #define label como a última coluna do DataFrame
+        X = data.iloc[:, :-1].to_numpy() #define X como uma matriz NumPy contendo todas as linhas e todas as colunas, exceto a última
+        y = data.iloc[:, -1].to_numpy() #define y como um array NumPy contendo todas as linhas e apenas a última coluna
 
-    elif features and not label:
-        features = data.columns
-        X = data.to_numpy()
-        y = None
+    elif features and not label: #se as features forem verdadeiras e as label falsas:
+        features = data.columns #define features como todas as colunas do DataFrame
+        X = data.to_numpy() #define X como uma matriz NumPy contendo todas as linhas e colunas do DataFrame
+        y = None #y é None
 
-    elif not features and label:
-        X = data.iloc[:, :-1].to_numpy()
-        y = data.iloc[:, -1].to_numpy()
-        features = None
-        label = None
+    elif not features and label: #se as features forem falsas e label for verdadeira:
+        X = data.iloc[:, :-1].to_numpy() #define X como uma matriz NumPy contendo todas as linhas e todas as colunas, exceto a última
+        y = data.iloc[:, -1].to_numpy() #define y como uma matriz NumPy contendo apenas a última coluna do DataFrame
+        features = None #features é None
+        label = None #label é None
 
-    else:
-        X = data.to_numpy()
-        y = None
-        features = None
-        label = None
+    else: #se as features e as labels forem falsas:
+        X = data.to_numpy() #define X como uma matriz NumPy contendo todos os dados do Dataframe
+        y = None #y é None 
+        features = None #features é None
+        label = None #label é None
 
-    return Dataset(X, y, features=features, label=label)
+    return Dataset(X, y, features=features, label=label) #devolve o dataset
 
 
 def write_csv(filename: str,
@@ -75,12 +76,15 @@ def write_csv(filename: str,
     label : bool, optional
         Whether the file has a label, by default False
     """
-    data = pd.DataFrame(dataset.X)
+    data = pd.DataFrame(dataset.X) #cria um DataFrame do pandas a partir do atributo X do objeto Dataset
 
-    if features:
-        data.columns = dataset.features
+    if features: #se as features forem verdadeiras:
+        data.columns = dataset.features #atribui os nomes das colunas do DataFrame (data.columns) para os nomes dos 
+        #do Dataset (dataset.features)
 
-    if label:
-        data[dataset.label] = dataset.y
+    if label: #se a label for verdadeira:
+        data[dataset.label] = dataset.y #adiciona uma coluna ao DataFrame chamada de acordo com a label do Dataset e 
+        #atribui a essa coluna os valores do atributo y do Dataset
 
-    data.to_csv(filename, sep=sep, index=False)
+    data.to_csv(filename, sep=sep, index=False) #escreve o DataFrame no arquivo CSV com o nome especificado (filename), 
+    #usando o separador definido (sep). O parâmetro index=False impede que o índice do DataFrame seja incluído no arquivo CSV
