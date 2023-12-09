bcc910096400aad0c23e1ffa54657706fdfe63a2
MarianaBragues <120012178+MarianaBragues@users.noreply.github.com>
2023-12-09 16:51:03 +0000

    losses descrito

--- src/si/neural_networks/losses.py
+++ src/si/neural_networks/losses.py
@@ -1,5 +1,5 @@
+#importação dos packages necessários:
 from abc import abstractmethod
-
 import numpy as np
 
 
@@ -65,7 +65,11 @@ class MeanSquaredError(LossFunction):
         float
             The loss value.
         """
+        #cálculo do erro quadrático médio entre as labels verdadeiras (y_true) e as labels preditos (y_pred). Subtrai os 
+        #dois arrays, eleva ao quadrado cada diferença e calcula a média desses quadrados. O resultado é o erro quadrático
+        #médio entre y_true e y_pred, que é devolvido como um número float
         return np.mean((y_true - y_pred) ** 2)
+
 
     def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
         """
@@ -83,7 +87,7 @@ class MeanSquaredError(LossFunction):
         numpy.ndarray
             The derivative of the loss function.
         """
-        # To avoid the additional multiplication by -1 just swap the y_pred and y_true.
+        #calculo da derivada da função de erro quadrático médio:
         return 2 * (y_pred - y_true) / y_true.size
 
 
@@ -91,7 +95,6 @@ class BinaryCrossEntropy(LossFunction):
     """
     Cross entropy loss function.
     """
-
     def loss(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
         """
         Compute the cross entropy loss function.
@@ -108,9 +111,14 @@ class BinaryCrossEntropy(LossFunction):
         float
             The loss value.
         """
-        # Avoid division by zero
+        #a variável p é definida como y_pred, mas com valores ajustados usando a função clip do NumPy para evitar valores 
+        #muito pequenos (próximos a zero) ou muito grandes (próximos a um), assim, evita-se problemas de logaritmos 
+        #próximos a zero ou indefinidos:
         p = np.clip(y_pred, 1e-15, 1 - 1e-15)
-        return -np.sum(y_true * np.log(p) + (1 - y_true) * np.log(1 - p))
+
+        return -np.sum(y_true * np.log(p) + (1 - y_true) * np.log(1 - p)) #calcula a entropia cruzada binária usando a 
+        #a fórmula matemática respetiva entre os rótulos verdadeiros (y_true) e os rótulos preditos ajustados (p) 
+
 
     def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
         """
@@ -128,9 +136,11 @@ class BinaryCrossEntropy(LossFunction):
         numpy.ndarray
             The derivative of the loss function.
         """
-        # Avoid division by zero
+        #a variável p é definida como y_pred, mas com valores ajustados usando a função clip do NumPy para evitar valores 
+        #muito pequenos (próximos a zero) ou muito grandes (próximos a um)
         p = np.clip(y_pred, 1e-15, 1 - 1e-15)
-        return - (y_true / p) + (1 - y_true) / (1 - p)
+
+        return - (y_true / p) + (1 - y_true) / (1 - p) #calculo da derivada da função de perda de entropia cruzada binária
     
 
 
@@ -138,7 +148,6 @@ class CategoricalCrossEntropy(LossFunction):
     """
     Categorical cross-entropy loss function.
     """
-
     def loss(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
         """
         Compute the categorical cross-entropy loss function.
@@ -155,9 +164,12 @@ class CategoricalCrossEntropy(LossFunction):
         float
             The loss value.
         """
-        # Avoid division by zero or taking the log of zero
+        #os valores de y_pred são ajustados usando a função clip do NumPy para evitar valores muito pequenos (próximos a 
+        #zero) ou muito grandes (próximos a um)
         y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
-        return -np.sum(y_true * np.log(y_pred))
+
+        return -np.sum(y_true * np.log(y_pred)) #calculo da entropia cruzada categórica
+
 
     def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
         """
@@ -175,6 +187,53 @@ class CategoricalCrossEntropy(LossFunction):
         numpy.ndarray
             The derivative of the loss function.
         """
-        # Avoid division by zero or taking the log of zero
+        #os valores de y_pred são ajustados usando a função clip do NumPy para evitar valores muito pequenos (próximos a 
+        #zero) ou muito grandes (próximos a um)
         y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
-        return - (y_true / y_pred)
+
+        return - (y_true / y_pred) #calculo da derivada da função de perda de entropia cruzada categórica
+    
+
+#Testes:
+if __name__ == '__main__':
+    from si.metrics import mse
+    #Teste para a classe MeanSquaredError
+    mse = MeanSquaredError()
+
+    #Teste para o cálculo do erro
+    y_true_mse = np.array([1, 2, 3])
+    y_pred_mse = np.array([1.5, 2.2, 2.8])
+    mse_value = mse.loss(y_true_mse, y_pred_mse)
+    print("Mean Squared Error:", mse_value)
+
+    #Teste para o cálculo da derivada do erro
+    mse_derivative = mse.derivative(y_true_mse, y_pred_mse)
+    print("Mean Squared Error Derivative:", mse_derivative)
+
+
+    #Testes para a classe BinaryCrossEntropy
+    bce = BinaryCrossEntropy()
+
+    #Teste para o cálculo do erro
+    y_true_bce = np.array([0, 1, 1])
+    y_pred_bce = np.array([0.2, 0.8, 0.9])
+    bce_value = bce.loss(y_true_bce, y_pred_bce)
+    print("Binary Cross Entropy:", bce_value)
+
+    #Teste para o cálculo da derivada do erro
+    bce_derivative = bce.derivative(y_true_bce, y_pred_bce)
+    print("Binary Cross Entropy Derivative:", bce_derivative)
+
+
+    #Testes para a classe CategoricalCrossEntropy
+    cce = CategoricalCrossEntropy()
+
+    #Teste para o cálculo do erro
+    y_true_cce = np.array([[0, 1, 0], [1, 0, 0]])
+    y_pred_cce = np.array([[0.1, 0.9, 0.4], [0.8, 0.1, 0.1]])
+    cce_value = cce.loss(y_true_cce, y_pred_cce)
+    print("Categorical Cross Entropy:", cce_value)
+
+    #Teste para o cálculo da derivada do erro
+    cce_derivative = cce.derivative(y_true_cce, y_pred_cce)
+    print("Categorical Cross Entropy Derivative:", cce_derivative)
